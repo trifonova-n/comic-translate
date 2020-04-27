a23d8e80841a61f800a6def4d672f7413916368d
Natalia Trifonova <koshmanova.n@gmail.com>
2020-04-26 18:32:28 -0700

    added SageMaker deployment script

--- deployment/deployment.py
+++ deployment/deployment.py
@@ -1,4 +1,12 @@
-
+from sagemaker.pytorch import PyTorchModel
+from sagemaker import get_execution_role
 
 def deploy():
-    pass
+    role = get_execution_role()
+    pytorch_model = PyTorchModel(model_data='s3://pytorch-sagemaker-example/model.tar.gz',
+                                 role=role,
+                                 entry_point='inference.py',
+                                 framework_version='1.3.1')
+
+    predictor = pytorch_model.deploy(initial_instance_count=1, instance_type='local')
+    return predictor
